6f15c402c578ac997e7322e5e4dbe053a9b57d53
Aditya Patel <aditya.06patel.05@gmail.com>
2025-04-29 17:00:04 +0530

    Java Collection Framework

---
+++ Stack/JCF/JCF.java
@@ -0,0 +1,16 @@
+import java.util.Stack;
+public class JCF{
+    public static void main(String[] args) {
+        Stack<Integer>s=new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+
+        while (!s.isEmpty()) {
+            System.out.println(s.peek());
+            s.pop();
+            
+        }
+        
+    }
+}
